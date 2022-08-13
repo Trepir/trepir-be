9b93267fb182bd1e3a2c8b4c903a45404384d1e4
Alejandro Fuchs <el.sandor.fuchs@gmail.com>
2022-08-13 16:32:53 +0200

    feat(be prisma module): add prisma module and update main ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService extends PrismaClient {
+	constructor() {
+		super({
+			datasources: {
+				db: {
+					url: 'postgres://postgres:123@localhost:5432/nest',
+				},
+			},
+		});
+	}
+}
