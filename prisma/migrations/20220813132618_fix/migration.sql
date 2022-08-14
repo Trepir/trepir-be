8993585a9f18165ff151dea0429771782ebd64ad
Alejandro Fuchs <el.sandor.fuchs@gmail.com>
2022-08-14 20:16:33 +0200

    feat(be activity module): implement create activity
    Added create route, service and controller. Updated the prisma schemas.

--- prisma/migrations/20220813132618_fix/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `TripId` on the `FavoriteActivity` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "FavoriteActivity" DROP COLUMN "TripId",
-ADD COLUMN     "tripId" TEXT;
---
+++ src/activity/activity.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ActivityDto } from './dto';
+
+@Injectable()
+export class ActivityService {
+	constructor(private prisma: PrismaService) {}
+
+	async create(dto: ActivityDto) {
+		const currentUser = await this.prisma.user.findUnique({
+			where: {
+				uid: dto.uid,
+			},
+		});
+
+		try {
+			const activity = await this.prisma.activity.create({
+				data: {
+					name: dto.name,
+					duration: dto.duration,
+					description: dto.description,
+					time: dto.time,
+					rating: dto.rating,
+					location: {
+						connectOrCreate: {
+							where: { googleId: dto.googleId },
+
+							create: {
+								latitude: dto.latitude,
+								longitude: dto.longitude,
+								googleId: dto.googleId,
+								country: dto.country,
+								state: dto.state,
+								city: dto.city,
+								locationName: dto.locationName,
+							},
+						},
+					},
+					tags: {},
+
+					creator: {
+						connect: {
+							id: currentUser.id,
+						},
+					},
+				},
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
+}
