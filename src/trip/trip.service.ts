63ebd2d37aeb975bede637ba9a4a3ce3c66f04af
Bernat <bernatpavon@gmail.com>
2022-08-17 22:43:54 +0200

    feat(trip service): get trip by Id

--- src/trip/trip.service.ts
+++ src/trip/trip.service.ts
@@ -2,7 +2,7 @@ import { Injectable, ForbiddenException } from '@nestjs/common';
 import { AccommodationService } from 'src/accommodation/accommodation.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TravelEventService } from 'src/travelEvent/travelEvent.service';
-import { TripDto, updateTripDto } from './dto';
+import { TripDto, updateTripDto, tripIdDto } from './dto';
 
 @Injectable()
 export class TripService {
