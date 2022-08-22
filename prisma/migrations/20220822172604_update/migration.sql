133b375dee8687135670d1e63e7364916a91da47
Alejandro Fuchs <el.sandor.fuchs@gmail.com>
2022-08-22 20:56:04 +0200

    fix(be schemas): modify prisma schemas

--- prisma/migrations/20220822172604_update/migration.sql
+++ prisma/migrations/20220822172604_update/migration.sql
@@ -57,9 +57,6 @@ CREATE TABLE "TripDayActivity" (
     "id" TEXT NOT NULL,
     "tripDayId" TEXT NOT NULL,
     "order" INTEGER NOT NULL,
-    "dayActivityId" TEXT,
-    "accommodationId" TEXT,
-    "travelEventId" TEXT,
 
     CONSTRAINT "TripDayActivity_pkey" PRIMARY KEY ("id")
 );
@@ -70,7 +67,7 @@ CREATE TABLE "DayActivity" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "activityId" TEXT NOT NULL,
     "time" TIMESTAMP(3),
-    "tripDayctivityId" TEXT,
+    "tripDayActivityId" TEXT,
 
     CONSTRAINT "DayActivity_pkey" PRIMARY KEY ("id")
 );
@@ -93,6 +90,7 @@ CREATE TABLE "accommodations" (
     "date" TIMESTAMP(3) NOT NULL,
     "state" "AccommodationState" NOT NULL,
     "eventType" "EventType" NOT NULL DEFAULT 'Accommodation',
+    "accommodationPairId" TEXT,
     "locationId" TEXT NOT NULL,
     "tripDayActivityId" TEXT,
 
@@ -105,10 +103,12 @@ CREATE TABLE "locations" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "latitude" DOUBLE PRECISION NOT NULL,
     "longitude" DOUBLE PRECISION NOT NULL,
-    "country" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
+    "country" TEXT,
+    "state" TEXT,
+    "photoUrl" TEXT[],
+    "formattedAddress" TEXT NOT NULL,
     "locationName" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
+    "city" TEXT,
     "googleId" TEXT,
 
     CONSTRAINT "locations_pkey" PRIMARY KEY ("id")
@@ -156,19 +156,22 @@ CREATE UNIQUE INDEX "users_uid_key" ON "users"("uid");
 CREATE UNIQUE INDEX "TripDayActivity_id_key" ON "TripDayActivity"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "TripDayActivity_dayActivityId_key" ON "TripDayActivity"("dayActivityId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "TripDayActivity_accommodationId_key" ON "TripDayActivity"("accommodationId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "TripDayActivity_travelEventId_key" ON "TripDayActivity"("travelEventId");
-
--- CreateIndex
 CREATE UNIQUE INDEX "DayActivity_id_key" ON "DayActivity"("id");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "DayActivity_tripDayActivityId_key" ON "DayActivity"("tripDayActivityId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accommodations_accommodationPairId_key" ON "accommodations"("accommodationPairId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accommodations_tripDayActivityId_key" ON "accommodations"("tripDayActivityId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "locations_googleId_key" ON "locations"("googleId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "travel_tripDayActivityId_key" ON "travel"("tripDayActivityId");
 
 -- AddForeignKey
 ALTER TABLE "trips" ADD CONSTRAINT "trips_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -180,16 +183,10 @@ ALTER TABLE "TripDay" ADD CONSTRAINT "TripDay_tripId_fkey" FOREIGN KEY ("tripId"
 ALTER TABLE "TripDayActivity" ADD CONSTRAINT "TripDayActivity_tripDayId_fkey" FOREIGN KEY ("tripDayId") REFERENCES "TripDay"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "TripDayActivity" ADD CONSTRAINT "TripDayActivity_dayActivityId_fkey" FOREIGN KEY ("dayActivityId") REFERENCES "DayActivity"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "TripDayActivity" ADD CONSTRAINT "TripDayActivity_accommodationId_fkey" FOREIGN KEY ("accommodationId") REFERENCES "accommodations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "TripDayActivity" ADD CONSTRAINT "TripDayActivity_travelEventId_fkey" FOREIGN KEY ("travelEventId") REFERENCES "travel"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "DayActivity" ADD CONSTRAINT "DayActivity_activityId_fkey" FOREIGN KEY ("activityId") REFERENCES "activities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DayActivity" ADD CONSTRAINT "DayActivity_tripDayActivityId_fkey" FOREIGN KEY ("tripDayActivityId") REFERENCES "TripDayActivity"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "favorites" ADD CONSTRAINT "favorites_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -201,13 +198,22 @@ ALTER TABLE "favorites" ADD CONSTRAINT "favorites_activityId_fkey" FOREIGN KEY (
 ALTER TABLE "favorites" ADD CONSTRAINT "favorites_tripId_fkey" FOREIGN KEY ("tripId") REFERENCES "trips"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "accommodations" ADD CONSTRAINT "accommodations_accommodationPairId_fkey" FOREIGN KEY ("accommodationPairId") REFERENCES "accommodations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "accommodations" ADD CONSTRAINT "accommodations_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "locations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "accommodations" ADD CONSTRAINT "accommodations_tripDayActivityId_fkey" FOREIGN KEY ("tripDayActivityId") REFERENCES "TripDayActivity"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "travel" ADD CONSTRAINT "travel_originLocationId_fkey" FOREIGN KEY ("originLocationId") REFERENCES "locations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "travel" ADD CONSTRAINT "travel_destinationLocationId_fkey" FOREIGN KEY ("destinationLocationId") REFERENCES "locations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "travel" ADD CONSTRAINT "travel_tripDayActivityId_fkey" FOREIGN KEY ("tripDayActivityId") REFERENCES "TripDayActivity"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "activities" ADD CONSTRAINT "activities_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "locations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
